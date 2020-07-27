5a46cc99babf878305bd68ea3a2cc5b0485ff73b
Stephen Ang <angssjr@g.ucla.edu>
2020-07-26 21:45:53 -0700

    first commit

---
+++ hw5.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const { allowedNodeEnvironmentFlags, report } = require('process');
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', exphbs());
+app.set('view engine', exphbs());
+app.set('port', 6140);
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+    console.log('hello world!');
+    res.render('main');
+})
+
+
+app.use((req,res)=>{
+    res.status(404);
+    res.render('404');   
+});
+
+app.use((req,res)=>{
+    res.status(500);
+    res.render('500');
+});
+
