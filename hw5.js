8f0781473433cab70b2170a9208239897062f3d0
Stephen Ang <angssjr@g.ucla.edu>
2020-07-26 23:15:11 -0700

    test1a

--- hw5.js
+++ hw5.js
@@ -3,9 +3,11 @@ const { allowedNodeEnvironmentFlags, report } = require('process');
 var app = express();
 
 var handlebars  = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 
-app.engine('handlebars', 'handlebars');
-app.set('view engine',handlebars);
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine','handlebars');
 app.set('port', 6140);
 
 app.use(express.urlencoded({extended:false}));
@@ -17,6 +19,33 @@ app.get('/', (req,res)=>{
     res.render('main');
 })
 
+app.get('/show-data', (req,res)=>{
+    var context = {};
+    context.sentData = req.query.myData;
+    res.render('show-data',context);
+})
+
+app.get('/get-loopback',(req,res)=>{
+    var qParams = "";
+    for(var p in req.query){
+        qParams+= "The name " + p + " contains the value " + req.query[p] + ", ";
+    }
+    qParams = qParams.substring(0, qParams.lastIndexOf(','));
+    qParams += '.';
+    var context = {};
+    context.dataList = qParams;
+    res.render('get-loopback', context);
+});
+
+app.get('/get-loopback-improved', (req,res)=>{
+    var qParams = [];
+    for(var p in req.query){
+        qParams.push({"name":p,"value":req.query[p]})
+    }
+    var context = {};
+    context.dataList = qParams;
+    res.render('get-loopback-improved',context);
+})
 
 app.use((req,res)=>{
     res.status(404);
