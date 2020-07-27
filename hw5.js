09a6bb950ec243809979e8a1c08f09f2ddd028de
Stephen Ang <angssjr@g.ucla.edu>
2020-07-26 23:34:27 -0700

    test1ab

--- hw5.js
+++ hw5.js
@@ -57,3 +57,14 @@ app.use((req,res)=>{
     res.render('500');
 });
 
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+})
+
+app.listen(app.get('port'),function(){
+    console.log('Express started on http://flip3.engr.oregonstate.edu'+app.get('port')+'; press Ctrl-C to terminate.')
+});
+    
